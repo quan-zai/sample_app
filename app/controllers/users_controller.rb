0d4cc782a04c235c98ba695819e85ff6b083faca
徐正权 <xuzhengquan@yundianjia.com>
2017-08-16 15:59:00 +0800

    sign up finish

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,6 +11,8 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       # 处理注册成功的情况
+      flash[:success] = "Welcome to the Sample App!"
+      redirect_to @user
     else
       render 'new'
     end
