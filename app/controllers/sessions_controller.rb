3b7d7361140b5a16dfba3b147fc7c09f26a8a6ab
徐正权 <xuzhengquan@yundianjia.com>
2017-08-17 14:31:07 +0800

    登录令牌

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,6 +7,7 @@ class SessionsController < ApplicationController
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
       log_in user
+      remember user
       redirect_to user
       # 登录用户, 然后重定向到用户的资料页面
     else
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+  attr_accessor :remember_token
+
   before_save { self.email = email.downcase }
   # before_save { self.email.downcase! }
 
@@ -8,5 +10,33 @@ class User < ApplicationRecord
   validates :email, presence: true, length: { maximum: 255 },
                     format: { with: VALID_EMAIL_REGEX },
                     uniqueness: { case_sensitive: false }
+  validates :email, presence: true, length: { minimum: 6 }
   has_secure_password
+
+  class << self
+    # 返回指定字符串的哈希摘要
+    def digest(string)
+      cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST : BCrypt::Engine.cost
+      BCrypt::Password.create(string, cost: cost)
+    end
+
+    # 返回一个随机令牌
+    def new_token
+      SecureRandom.urlsafe_base64
+    end
+
+    def remember
+      self.remember_token = self.new_token
+      update_attribute(:remember_digest, self.digest(remember_token))
+    end
+
+    def authenticated?(remember_token)
+      BCrypt::Password.new(remember_digest).is_password?(remember_token)
+    end
+
+    #忘记用户
+    def forget
+      update_attribute(:remember_digest, nil)
+    end
+  end
 end
